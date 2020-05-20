68ee55999bd8a9751ffcb78074f9b76fffb72639
MFY2000 <fa19bsse0014@maju.edu.pk>
2020-05-20 17:21:09 +0500

    20/5/2020 5:20 pm

--- Filing/Syntax/CreateFile.java
+++ Filing/Syntax/CreateFile.java
@@ -1,11 +1,41 @@
-
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
 
 public class CreateFile {
     public static void main(String[] args) {
         System.out.print("\033[H\033[2J"); // clear console screen 
-
-
-
         
+        try {
+            File fileName = null;
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Enter the method (demo = 1,runtime = 2, Exit = 0): ");
+            int key = scan.nextInt(); 
+            if(key == 0){ System.exit(1); }
+            
+            switch (key) {
+                case 1:
+                    fileName = new File("firstFile.txt");
+                    break;
+                case 2:
+                    scan.nextLine();
+                    System.out.print("Enter the name of File: ");
+                    String name = scan.nextLine();
+                    
+                    System.out.print("Enter the extension of File: ");
+                    String extension = scan.nextLine();
+                    fileName = new File(name+"."+extension);
+                    break;
+
+                default:
+                    break;
+            }
+            if(fileName.createNewFile())
+                System.out.println("Succesfully Created the file");
+        }
+         catch (Exception e) {
+                System.out.println(e.getMessage());
+        }
+    
     }
 }
