9603e8a09c8514ad24ac7fe4ac5c916abbeb093e
MFY2000 <fa19bsse0014@maju.edu.pk>
2020-05-21 14:34:29 +0500

    21/5/2020 2:34 pm

--- Filing/Syntax/Readfile.java
+++ Filing/Syntax/Readfile.java
@@ -2,7 +2,26 @@
 public class Readfile {
     public static void main(String[] args) {
         System.out.print("\033[H\033[2J"); // clear console screen 
+B obj = new B();
+System.out.println(obj.unfinished());
 
         System.out.println("Hello world");
     }    
+
+
+
 }
+
+abstract class A{
+    abstract  int unfinished();
+}
+
+
+class B extends A{
+    public int unfinished(){
+        return 12;
+    }
+}
+
+
+
--- Filing/Syntax/Writefile.java
+++ Filing/Syntax/Writefile.java
@@ -1,4 +1,31 @@
 
 public class Writefile {
-    
+    public static void main(String[] args) {
+        B obj = new B();
+        System.out.println(obj.unfinished(12,15));
+    }    
+
+
+
 }
+
+abstract class A{
+    A(){
+        System.out.println("Constructors");
+    }
+    int unfinished(int a, int b){
+        return (a+b);
+    }
+
+}
+
+
+class B extends A{
+
+    B(){
+        super();
+    }
+}
+
+
+
