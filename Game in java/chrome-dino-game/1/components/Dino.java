3ba7e59fca5a8ba6f8d49cdd374dd4fbeed98a38
MFY2000 <fa19bsse0014@maju.edu.pk>
2020-05-23 21:05:34 +0500

    23/5/2020 9:05 pm

--- Game in java/chrome-dino-game/1/components/Dino.java
+++ Game in java/chrome-dino-game/1/components/Dino.java
@@ -6,6 +6,7 @@ import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 import components.Ground;
+
 import javafx.scene.paint.Color;
 import utility.Resource;
 
@@ -16,14 +17,9 @@ public class Dino {
   private static boolean topPointReached;
   private static int jumpFactor = 20;
 
-  public static final int STAND_STILL = 1,
-                    RUNNING = 2,
-                    JUMPING = 3,
-                    DIE = 4;
-  private final int LEFT_FOOT = 1,
-                    RIGHT_FOOT = 2,
-                    NO_FOOT = 3;
-  
+  public static final int STAND_STILL = 1, RUNNING = 2, JUMPING = 3, DIE = 4;
+  private final int LEFT_FOOT = 1, RIGHT_FOOT = 2, NO_FOOT = 3;
+
   private static int state;
 
   private int foot;
@@ -54,7 +50,7 @@ public class Dino {
 
     // g.drawRect(getDino().x, getDino().y, getDino().width, getDino().height);
 
-    switch(state) {
+    switch (state) {
 
       case STAND_STILL:
         System.out.println("stand");
@@ -62,10 +58,10 @@ public class Dino {
         break;
 
       case RUNNING:
-        if(foot == NO_FOOT) {
+        if (foot == NO_FOOT) {
           foot = LEFT_FOOT;
           g.drawImage(leftFootDino, dinoStartX, dinoTopY, null);
-        } else if(foot == LEFT_FOOT) {
+        } else if (foot == LEFT_FOOT) {
           foot = RIGHT_FOOT;
           g.drawImage(rightFootDino, dinoStartX, dinoTopY, null);
         } else {
@@ -75,27 +71,27 @@ public class Dino {
         break;
 
       case JUMPING:
-        if(dinoTop > topPoint && !topPointReached) {
+        if (dinoTop > topPoint && !topPointReached) {
           g.drawImage(image, dinoStartX, dinoTop -= jumpFactor, null);
           break;
-        } 
-        if(dinoTop >= topPoint && !topPointReached) {
+        }
+        if (dinoTop >= topPoint && !topPointReached) {
           topPointReached = true;
           g.drawImage(image, dinoStartX, dinoTop += jumpFactor, null);
           break;
-        }         
-        if(dinoTop > topPoint && topPointReached) {      
-          if(dinoTopY == dinoTop && topPointReached) {
+        }
+        if (dinoTop > topPoint && topPointReached) {
+          if (dinoTopY == dinoTop && topPointReached) {
             state = RUNNING;
             topPointReached = false;
             break;
-          }    
-          g.drawImage(image, dinoStartX, dinoTop += jumpFactor, null);          
+          }
+          g.drawImage(image, dinoStartX, dinoTop += jumpFactor, null);
           break;
         }
-      case DIE: 
-        g.drawImage(deadDino, dinoStartX, dinoTop, null);    
-        break;     
+      case DIE:
+        g.drawImage(deadDino, dinoStartX, dinoTop, null);
+        break;
     }
   }
 
@@ -107,9 +103,12 @@ public class Dino {
     Rectangle dino = new Rectangle();
     dino.x = dinoStartX;
 
-    if(state == JUMPING && !topPointReached) dino.y = dinoTop - jumpFactor;
-    else if(state == JUMPING && topPointReached) dino.y = dinoTop + jumpFactor;
-    else if(state != JUMPING) dino.y = dinoTop;
+    if (state == JUMPING && !topPointReached)
+      dino.y = dinoTop - jumpFactor;
+    else if (state == JUMPING && topPointReached)
+      dino.y = dinoTop + jumpFactor;
+    else if (state != JUMPING)
+      dino.y = dinoTop;
 
     dino.width = image.getWidth();
     dino.height = image.getHeight();
