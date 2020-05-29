9df6021799c085bb4be68994aa5f6d68e18fde01
MFY2000 <fa19bsse0014@maju.edu.pk>
2020-05-30 01:58:22 +0500

    30/5/2020 1:58 am

---
+++ Game in java/untitled/src/Todo/Optionlist.java
@@ -0,0 +1,64 @@
+package Todo;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+
+public class Optionlist implements Initializable {
+
+    private Connection con;
+    private Statement sc;
+    private ResultSet res;
+
+    @FXML
+    public ChoiceBox Choice;
+    ObservableList<String> ChoiceList;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        databaseChoicelist();
+        Choice.setItems(ChoiceList);
+        Choice.setValue("MM");
+    }
+
+    private void databaseChoicelist(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/database_choicelist?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","");
+            sc = con.createStatement();
+            showdata();
+        }
+        catch (Exception e){
+            System.out.println("Error : "+e);
+        }
+        System.out.println("123");
+    }
+
+    private void showdata(){
+
+        String[] Feild = new String[10];
+        try {
+            String query = "select * from subjectlist";
+            res = sc.executeQuery(query);
+            int i=0;
+            while (res.next()){
+                Feild[i] = res.getString("Field");
+                i++;
+            }}
+        catch (Exception e){
+            System.out.println("Error : "+e);
+        }
+        ChoiceList = FXCollections.observableArrayList(Feild);
+    }
+
+}
